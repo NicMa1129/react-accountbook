31ae2221c7382e71af0b46a386b5dc4a3824125b
Nic Ma <Nic.Ma@farfetch.com>
2019-07-05 11:49:05 +0800

    add test2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './static/style/main.scss'
 import './static/lib/font-awesome-4.7.0/css/font-awesome.min.css'
 var vConsole = new VConsole();
 console.log('test1')
+console.log('test2')
 ReactDOM.render(
     routes,
     document.getElementById('root')
