379899f521490623a112c9e00e38dbce39433e48
Nic Ma <Nic.Ma@farfetch.com>
2019-07-12 14:09:06 +0800

    1 add by master

--- src/index.js
+++ src/index.js
@@ -6,16 +6,7 @@ import VConsole from 'vconsole'
 import './static/style/main.scss'
 import './static/lib/font-awesome-4.7.0/css/font-awesome.min.css'
 var vConsole = new VConsole();
-console.log('test1')
-console.log('test2')
-console.log('test3')
-console.log('test4')
-console.log('test6')
-console.log('test5')
-console.log('test7')
-console.log('test8')
-console.log('test9')
-console.log('test12')
+console.log('1 add by master')
 ReactDOM.render(
     routes,
     document.getElementById('root')
