af66bdea8e04f382bbdeb891722ba32e7bcbe4fb
nic.ma <nic.ma@farfetch.com>
2019-09-10 16:23:15 +0800

    2 add by test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './static/style/main.scss'
 import './static/lib/font-awesome-4.7.0/css/font-awesome.min.css'
 var vConsole = new VConsole();
 console.log('1 update by test');
+console.log('2 add by test');
 ReactDOM.render(
     routes,
     document.getElementById('root')
